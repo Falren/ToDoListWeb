2472fc60b05c5a547c9e7e22b17755aec37cbec6
Falren <nokuchie@gmail.com>
2020-10-26 11:39:06 +0200

    setup angular project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ToDoListWeb'`, () => {
+  it(`should have as title 'Hello'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ToDoListWeb');
+    expect(app.title).toEqual('Hello');
   });
 
   it('should render title', () => {
